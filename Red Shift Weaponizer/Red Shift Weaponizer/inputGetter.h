e57053b304038009ec033c77a816ee0f2d73cada
brandonjpb <brandon@sterba.net>
2025-02-16 11:21:13 -0800

    Update inputGetter.h
    Cleaned up unneeded libraries

--- Red Shift Weaponizer/Red Shift Weaponizer/inputGetter.h
+++ Red Shift Weaponizer/Red Shift Weaponizer/inputGetter.h
@@ -1,12 +1,8 @@
 #pragma once
 
-#include <cstdlib>
-#include <string>
-#include <iomanip>
+
 #include <iostream>
-#include <istream>
 #include <limits>
-#include <algorithm>
 
 int getFrameInput();
 int getReceiverInput();
