818b68e51dacf0d002708f762f5b4ddda9e317a5
brandonjpb <brandon@sterba.net>
2025-05-03 00:55:17 -0700

    Delete SWeARS (Simplified Weapon Assembler for Red Shift) directory

--- SWeARS (Simplified Weapon Assembler for Red Shift)/SWeARS (Simplified Weapon Assembler for Red Shift)/iofile.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "structs.h"
-#include <string>
-
-void saveWeaponToTextFile(const Weapon& weapon, const std::string& filename);
--- SWeARS (Simplified Weapon Assembler for Red Shift)/SWeARS (Simplified Weapon Assembler for Red Shift)/weapongen.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "structs.h"
-#include "partdatabase.h"
-
-Weapon generateWeapon(const PartDatabase& db);
-bool isCompatible(const Part& a, const Part& b);
